635d766b799e441f41106b778e332390d3139bd0
Sidoupiar <sidoupiar@qq.com>
2020-12-24 22:09:16 +0800

    更新版本

--- zprototype/entities.lua
+++ zprototype/entities.lua
@@ -10,14 +10,16 @@ local source = SIPackers.EnergySource( SITypes.energy.electric )
 
 SIEXIT.labMk2Name = SIGen.NewLab( SIEXIT.labMk2Name )
 .SetProperties( 5 , 5 , 600 , 2 , "320KW" , source )
-.SetPluginData( 3 , { 0 , 0.9 } )
+.SetPluginData( 3 )
 .SetCorpse( "lab-remnants" , "lab-explosion" )
+.SetPluginTypes{ SITypes.moduleEffect.speed , SITypes.moduleEffect.product , SITypes.moduleEffect.consumption , SITypes.moduleEffect.pollut }
 .GetCurrentEntityName()
 SIEXIT.labMk2ItemName = SIGen.GetCurrentEntityItemName()
 
 SIEXIT.labMk3Name = SIGen.NewLab( SIEXIT.labMk3Name )
 .SetProperties( 7 , 7 , 1500 , 3 , "2MW" , source )
-.SetPluginData( 4 , { 0 , 0.9 } )
+.SetPluginData( 4 )
 .SetCorpse( "lab-remnants" , "lab-explosion" )
+.SetPluginTypes{ SITypes.moduleEffect.speed , SITypes.moduleEffect.product , SITypes.moduleEffect.consumption , SITypes.moduleEffect.pollut }
 .GetCurrentEntityName()
 SIEXIT.labMk3ItemName = SIGen.GetCurrentEntityItemName()
