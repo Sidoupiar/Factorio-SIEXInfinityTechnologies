5e816f57cdb806f67904ac044944767bb20eed9b
Sidoupiar <sidoupiar@qq.com>
2021-05-18 23:00:20 +0800

    提高兼容性

--- zprototype/entities.lua
+++ zprototype/entities.lua
@@ -13,6 +13,7 @@ SIEXIT.labMk2Name = SIGen.NewLab( SIEXIT.labMk2Name )
 .SetPluginData( 3 )
 .SetCorpse( "lab-remnants" , "lab-explosion" )
 .SetPluginTypes{ SITypes.moduleEffect.speed , SITypes.moduleEffect.product , SITypes.moduleEffect.consumption , SITypes.moduleEffect.pollut }
+.SetCustomData{ inputs = {} }
 .FillImage()
 .GetCurrentEntityName()
 SIEXIT.labMk2ItemName = SIGen.GetCurrentEntityItemName()
@@ -22,6 +23,7 @@ SIEXIT.labMk3Name = SIGen.NewLab( SIEXIT.labMk3Name )
 .SetPluginData( 4 )
 .SetCorpse( "lab-remnants" , "lab-explosion" )
 .SetPluginTypes{ SITypes.moduleEffect.speed , SITypes.moduleEffect.product , SITypes.moduleEffect.consumption , SITypes.moduleEffect.pollut }
+.SetCustomData{ inputs = {} }
 .FillImage()
 .GetCurrentEntityName()
 SIEXIT.labMk3ItemName = SIGen.GetCurrentEntityItemName()
